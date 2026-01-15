3e1be5e08d7d3dbddf3f99e3cab4d18d6cdd6d9c
Ritesh <rfauzdar6@gmail.com>
2026-01-15 12:30:21 +0530

    created the logout function

--- backend/src/Models/user.model.js
+++ backend/src/Models/user.model.js
@@ -33,10 +33,10 @@ const userSchema = new Schema({
 )
 
 // before saving any password we need to hash it
-userSchema.pre("save", async function(next) {
-    if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+
     this.password = await bcrypt.hash(this.password, 10);
-    next();
 });
 
 // compare passwords
--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -65,4 +65,21 @@ const loginUser = async(req,res) => {
     }
 }
 
-export {registerUser, loginUser};
+const logoutUser = async(req,res) => {
+    try {
+        const {email} = req.body;
+
+        const user = await User.findOne({email});
+
+        if(!user) return res.status(400).json({
+            message: "User not found"
+        })
+
+        res.status(200).json({
+            message: "Logout successful"
+        });
+    } catch (error) {
+        res.status(500).json({message: "Internal Server Error", error: error.message});
+    }
+}
+export {registerUser, loginUser, logoutUser};
